1adfbfaf24aada700634feda6f579d77dff65c7c
chezze911 <chinguyen@Chis-MacBook-Pro.local>
2017-06-13 17:41:45 -0700

    Update error handling with JSON P

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -46,6 +46,10 @@ function loadData() {
     // Wikipedia AJAX request goes here
     var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + cityStr + '&format=json&callback=wikiCallback';
 
+    var wikiRequestTimeout = setTimeout(function(){
+        $wikiElem.text("failed to get wikipedia resources");
+    }, 8000);
+
     $.ajax({
         url: wikiUrl,
         dataType: "jsonp",
@@ -58,6 +62,8 @@ function loadData() {
                 var url = 'http://en.wikipedia.org/wiki/' + articleStr;
                 $wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
             };
+
+            clearTimeout(wikiRequestTimeout);
         }
     });
     return false;
